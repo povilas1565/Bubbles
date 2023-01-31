bcdcd192a6545a5b64b980e6c96966d75d721c38
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-01-31 23:01:10 +0300

    Созданы модели(models).

---
+++ Assets/Scripts/Models/BubbleModel.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Bubbles;
+public sealed class BubbleModel
+{
+    private bool _active;
+    private float _speed;
+    private float _radius;
+
+    public bool Active
+    {
+        get => _active;
+        set => _active = value;
+    }
+    
+    private float Speed
+    {
+        get => _speed;
+        set => _speed = value;
+    }
+
+    public float Radius
+    {
+        get => _radius;
+        set => _radius = value;
+    }
+}
+}
+
+
+    
