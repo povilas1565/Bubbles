ffe831516a6a65f1ca78be365fc645968c652356
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-02-02 12:33:51 +0300

    Cоздана основа (core).

---
+++ Assets/Scripts/Core/Singleton.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Bubbles
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        #region Fields
+
+        private static bool _shuttingDown = false;
+        private static object _lock = new object();
+        private static T_instance;
+
+        #endregion
+
+        #region Properties
+
+        public static T Instance
+        {
+            get
+            {
+                if (_shuttingDown)
+                {
+                    Debug.LogWarning("[Singleton] Instance '" + typeof(T) + "' already destroyed. Returning null.");
+                    return null;
+                }
+
+                lock (_lock)
+                {
+                    if (_instance != null) return _instance;
+                    _instance = (T)FindObjectOfType(typeof(T));
+                    if (_instance != null) return _instance;
+                    var singletonObject = new GameObject();
+                    _instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T) + " (Singleton)";
+
+                    DontDestroyOnLoad(singletonObject);
+                    return _instance;
+                }
+            }
+        }
+
+        #endregion
+
+        #region UnityMethods
+
+        private void OnApplicationQuit()
+        {
+            _shuttingDown = true;
+        }
+
+        private void OnDestroy()
+        {
+            _shuttingDown = true;
+        }
+
+        #endregion
+    }
+}
